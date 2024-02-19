5ba56da13580a2974ae70ebc438a04f4be200473
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-19 20:54:59 +0000

    variable commit changes

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+// we cant assign a value to a variable declared by const keyword.
+const userId = 1234;
+
+// prefer not to use because of the issues in block scope and functionl scope.
+var emailId = "jai@mahakal.com";
+
+// it has block scope and the value can be changed.
+let userPassword = "12347890";
+userPassword = "2222222"
+
+/* (please dont wwrite like this , it wont throw an error but there is a reason why we
+use let and const) */
+placeToworship = "Mandir";
+
+// in let we can reassign the value but not re-declare the same variable name
+let humanity;
+
+console.table([userId, emailId, userPassword, placeToworship, humanity]);
+
