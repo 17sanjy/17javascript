b1a7118dabe6f37b24f610f642f4cb7b7f0305fa
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-20 12:47:15 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"  // to make older version compatible with newer version.
+
+/* 
+by usinfg datatypes we specifies the intances a place to store data in the memory
+
+There are 8 types of data in js among those 7 are premitive data Types.
+
+1. Number - whole number 123,
+2. BigInt - large number 1234........n,
+3. String - set of characters "Sanjay",
+4. Null - special standalone value but type of Object,
+5. undefined - declared but not initialized,
+6. Boolean - True/False,
+7. Symbol - for uniqueness and it is mutable only
+
+8. Object // Non-Premitive
+*/
