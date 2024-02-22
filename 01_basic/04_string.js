ccaa2023f1b5b1b2db72beb29ff9afb63c546e87
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-22 11:21:50 +0000

    some string methods

---
+++ 01_basic/04_string.js
@@ -0,0 +1,27 @@
+let name = "sanjay";
+let age = 20;
+// console.log(`username is ${name} and his age is ${age}`);
+
+let  userName = new String("The-quick-brown-fox");
+let regex = /[A-Z]/g;
+// console.log(userName);
+// console.log(userName.match(regex));
+
+// console.log(userName.charAt(3));
+// console.log(userName.indexOf('y'));
+// console.log(userName.length);
+// console.log(userName.endsWith('y'));
+// console.log(userName.startsWith('s'));
+// console.log(userName.includes('jay'));
+// console.log(userName.at(-7));
+console.log(userName.slice(-4, 2));
+// console.log(userName.trim(userName));
+
+let character = "sanjay-shandilya-kumar";
+// console.log((character.split("-", 2)));
+
+let trimChar = "sanjayshandliyakumar"
+let trimChar2 = "       sanjayshandliyakumar      ";
+
+// console.log(trimChar);
+// console.log(trimChar2.trim());
