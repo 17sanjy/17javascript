2cde7a1c3d6c110033d07181a1ec42c90dc0dbe5
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-26 19:15:32 +0000

    merge, flat, isArray, from

---
+++ 02_basic/02_array.js
@@ -0,0 +1,23 @@
+let marvel = ["Thor", "Iron-Man", "Spider-Man"];
+let dc = ["Super-Man", "Bat-Man", "Aqua-Man"];
+
+// marvel.push(dc);
+// console.log(marvel);
+// console.log(marvel[3][2]);
+
+let all = marvel.concat(dc);  // merged both
+// console.log(all);
+
+ let spreadOp = [...marvel, ...dc]; // merged both with spread op
+//  console.log(spreadOp);
+
+
+let arr = [1, 2, 3, [2, 4, [8, 9]], 7, [8, 7, 9, [1, 0]]]
+let len = arr.length;
+// console.log(len); // 6
+
+let flatMethod = arr.flat(Infinity);
+// console.log(flatMethod);
+
+console.log(Array.isArray("Bhola")); // false
+console.log(Array.from("Bhola")); // [ 'B', 'h', 'o', 'l', 'a' ]
