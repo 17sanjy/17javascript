92827e747bada2fed06b70b767361afc3cb84cf1
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-26 14:06:38 +0000

    another way of getting date in milliseconds

---
+++ 01_basic/06_date_and_time.js
@@ -0,0 +1,17 @@
+let dob = new Date();
+
+// console.log(dob.getDate());
+// console.log(dob.getDay());
+// console.log(dob.getFullYear());
+// console.log(dob.getHours());
+// console.log(dob.getMilliseconds());
+// console.log(dob.getMinutes());
+// console.log(dob.getMonth());
+// console.log(dob.getSeconds());
+// console.log(dob.getTime());
+// console.log(dob.toISOString());
+// console.log(dob.toString());
+// console.log(dob.toLocaleDateString());
+
+let timeStamp = Date.now();
+console.log(timeStamp);
