4d96c2922317d043cab7e95edb5ce59aea08c7eb
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-26 19:19:37 +0000

    Array.of method

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -19,5 +19,11 @@ let len = arr.length;
 let flatMethod = arr.flat(Infinity);
 // console.log(flatMethod);
 
-console.log(Array.isArray("Bhola")); // false
-console.log(Array.from("Bhola")); // [ 'B', 'h', 'o', 'l', 'a' ]
+// console.log(Array.isArray("Bhola")); // false
+// console.log(Array.from("Bhola")); // [ 'B', 'h', 'o', 'l', 'a' ]
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // [ 100, 200, 300 ]: converts into array
