9da0d7599b0fb8412ec230751d3df1fcd7e817bc
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-28 13:45:21 +0000

    object property

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,34 @@
+const zehrUser = new Object();
+
+zehrUser.id = "123abc"
+zehrUser.name = "heyword";
+zehrUser.email = "zehr@zehr.com"
+// console.log(zehrUser);
+
+const zehrTwo = {
+    email: "zehrTwo",
+    fullName: {
+        firstName: "Zehr",
+        lastName : "Two"
+    }
+}
+// console.log(zehrTwo.fullName.lastName);
+
+//merging two objects
+
+const obj1 = { name: "Tom", title: "Holland"};
+const obj2 = { firstName: "Chris", lastName: "Evan"};
+
+// keys of both objects should be unique otherwise it overlap and return the latest key value
+// const obj3 = Object.assign({}, obj1, obj2);
+
+// or
+
+const obj3 = { ...obj1, ...obj2};
+
+// console.log(obj3);
+
+console.log(Object.keys(zehrUser)); // return only keys of the object
+console.log(Object.values(zehrUser)); //return only values
+
+console.log(zehrUser.hasOwnProperty('email')); // to check if key exist or not
