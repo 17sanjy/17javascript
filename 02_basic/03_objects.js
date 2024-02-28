baf0728cbe7b46a9aa63be255eaffe1d661beda4
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-28 07:08:47 +0000

    object literal

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,13 @@
+/* singleton in js : is a design pattern that tells us we can create only one insatnce of 
+a class that can be acccessed globally. hwn we create an object with contrictor then only
+singleton can be utilized that solves commonly problems to all components. If we create 
+object with object literal then we cannot define singleton. */
+
+let userMe = {
+    name: "Ironman",
+    age: 24,
+    email: "ironman@marvel.com",
+    location: "New York",
+    isLoggedIn: true,
+    lastLoginDays: ["Monday", "Tuesday"]
+}
