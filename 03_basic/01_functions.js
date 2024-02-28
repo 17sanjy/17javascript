58a70170f245e47c44a2c60536b6f03f814c9aab
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-02-28 19:19:55 +0000

    function with objects and array, rest operator

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -23,4 +23,28 @@ function myFunction2(username = "Spider-Man"){
     return (`${username} has just logged in`);
 }
 let result2 = myFunction2()
-console.log(result2);
+// console.log(result2);
+
+// ++++++++++++++++++++++++++++functions in depth++++++++++++++++++++++++++
+
+const user = {
+    username: "Loki",
+    power: "Mystic"
+}
+
+function myNewFunction(data){
+    return `User is ${data.username} and He has ${data.power} power`;
+}
+console.log(myNewFunction(user));
+
+function manyValue(value1, value2, ...value){
+    return value;
+}
+console.log(manyValue(3, 4, 8, 9, 10));
+
+const newArray = [1, 2, 3, 4, 8, 9];
+
+function newArrayFunction(...arr){
+    return arr
+}
+console.log(newArray[1]);
