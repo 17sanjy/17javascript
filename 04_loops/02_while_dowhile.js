78eeca684d09c08f11aedf04ddb3c173c8b4ba80
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-03-01 18:50:17 +0000

    while, do while loop

---
+++ 04_loops/02_while_dowhile.js
@@ -0,0 +1,14 @@
+let index = 0;
+
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArr = ["dc", "sony", "marvel"];
+
+let arr = 0;
+while (arr < myArr.length) {
+    console.log(myArr[arr]);
+    arr++;
+}
