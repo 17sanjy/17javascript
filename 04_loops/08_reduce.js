8c8cf0a762eaa9769f1a8ee9f02386f646be5740
Sanjay Mahto <32986804+17sanjy@users.noreply.github.com>
2024-03-02 17:06:36 +0000

    reduce

--- 04_loops/08_reduce.js
+++ 04_loops/08_reduce.js
@@ -1,7 +1,18 @@
 let myNum = [1, 2, 3, 4];
 
 let sum = myNum.reduce((acc, currVal) => {
+    // console.log(`accumulator: ${acc} and current Value: ${currVal}`);
     return acc + currVal;
 }, 0);
 
-console.log(sum);
+// console.log(sum);
+
+let courses = [
+    { courseName: "HTML", price: 499},
+    { courseName: "CSS", price: 499},
+    { courseName: "JavaScript", price: 1499},
+    { courseName: "React", price: 4999},
+];
+
+let TotalPrice = courses.reduce((acc, course) => acc + course.price, 0);
+console.log(TotalPrice);
